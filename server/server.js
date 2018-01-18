7d4249e6861cb5e8a4a10fb2074b9023d9db86d2
Beauwie <kvn.philippe@gmail.com>
2018-01-18 10:36:29 +0100

    another try

--- server/server.js
+++ server/server.js
@@ -34,25 +34,20 @@ app.use(express.static(publicPath));
 app.use(function(req, res, next) {
   //   // Website you wish to allow to connect
   //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-
   //   // Request methods you wish to allow
   //   res.setHeader(
   //     "Access-Control-Allow-Methods",
   //     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   //   );
-
   //   // Request headers you wish to allow
   //   res.setHeader(
   //     "Access-Control-Allow-Headers",
   //     "X-Requested-With,content-type"
   //   );
-
-  res.setHeader("Access-Control-Allow-Headers", "x-auth, content-type");
-
+  // res.setHeader("Access-Control-Allow-Headers", "x-auth, content-type");
   //   // Set to true if you need the website to include cookies in the requests sent
   //   // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
+  // res.setHeader("Access-Control-Allow-Credentials", true);
   //   // Pass to next layer of middleware
   //   next();
 });
